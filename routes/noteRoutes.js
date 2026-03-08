d7f674de66a17e44d8539d600c608440cf9e385b
agbgaming24 <aryan2446.pis@gmail.com>
2026-03-08 21:45:44 +0530

    url correction

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -9,6 +9,6 @@ router.post('/notes/check-grammar',noteController.checkGrammar);
 router.post('/notes',noteController.saveNote)
 router.get('/notes',noteController.getNote)
 router.get('/notes/:id/render',noteController.renderNote)
-router.post('/upload', upload.single('file'), noteController.uploadNote)
+router.post('/notes/upload', upload.single('file'), noteController.uploadNote)
 
 export default router
